15fe7283a16af9eb870041c8be1dfbcc9b2d457a
Dana Palmer <roiai4@hotmail.com>
2018-06-29 00:39:24 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,19 +4,22 @@ class Dog
     @name = name
   end
   
-  def breed(breed)
-    @breed = breed
-  end
-  def breed
-    @breed
-  end
-
   def name
     @name
   end
 
   def name=(new_name)
     @name = new_name
+  
+  def breed(breed)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end
+
+
   end
 end
 
