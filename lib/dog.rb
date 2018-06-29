2d223e70910ced2a5af00fb4f562bdca26c5e5d3
Dana Palmer <roiai4@hotmail.com>
2018-06-29 00:31:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,14 @@
 class Dog 
   
-  def initialize_dog(name, breed)
+  def name(name, breed)
     @dog_nameiv = name
-    @breediv = breed
   end
   
-  def dog_name(name)
+  def name(name)
     @dog_nameiv
   end
 
-  def dog_name=(new_name)
+  def name=(new_name)
     @dog_nameiv = new_name
   end
 end
